025c67e7792ad29c0c99754e54e5266cc65b4bc7
Samuel Adewuyi <samueladewuyi.oa@gmail.com>
2023-06-30 13:03:02 +0100

    corrected npm build error

--- src/body/body.jsx
+++ src/body/body.jsx
@@ -14,13 +14,13 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
                 <FontAwesomeIcon icon={faBars} /> 
                 </a>
                 </a>  
-                        <a href className="nav__search-icon" >  
+                        <div className="nav__search-icon" >  
                            <a href="#" className="nav__search"> Search Products & brands 
                            <a href="#" className="nav__search-icon2">
                            <FontAwesomeIcon icon={faMagnifyingGlass} />
                            </a>
                            </a>            
-                        </a>   
+                        </div>   
             <u1 className="nav__menu">
                 <li className="nav__item"><a href className="nav__link">Download App</a></li>
                 <li className="nav__item"><a href className="nav__link">Signup/login</a></li>
