f6c3d1625b31bb9a999effde2bf5d91a935e656e
Samuel Adewuyi <samueladewuyi.oa@gmail.com>
2023-07-20 19:48:31 +0100

    made the website mobile responsive

--- src/body/body.jsx
+++ src/body/body.jsx
@@ -1,41 +1,78 @@
-import React from "react";
-import './body.css';
-import { FaBars } from "react-icons/fa";
-import { FaSistrix } from "react-icons/fa";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faBars } from "@fortawesome/free-solid-svg-icons";
-// import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+// import React from "react";
+// import './body.css';
+// import { FaBars } from "react-icons/fa";
+// import { FaSistrix } from "react-icons/fa";
 
 
 
-    function Body () {
-    return(
-        <div className="nav">
-           <a href="#" className="nav__brand">
-                Shopio <a href="shopio" className="nav__brand-icon">
-                    <FaBars className="nav__brand-icon" />
-                {/* <FontAwesomeIcon icon={faBars} />  */}
-                </a>
-                </a>  
-                        <div className="nav__search-icon" >  
-                           <a href="#" className="nav__search"> Search Products & brands 
-                           <a href="#" className="nav__search-icon2">
-                            <FaSistrix className="nav__search-icon2" />
-                           {/* <FontAwesomeIcon icon={faMagnifyingGlass} /> */}
-                           </a>
-                           </a>            
-                        </div>   
-            <u1 className="nav__menu">
-                <li className="nav__item"><a href className="nav__link">Download App</a></li>
-                <li className="nav__item"><a href className="nav__link">Signup/login</a></li>
-            </u1>
+
+//     function Body () {
+//     return(
+//         <div className="nav">
+//            <a href="#" className="nav__brand">
+//                 Shopio <a href="shopio" className="nav__brand-icon">
+//                     <FaBars className="nav__brand-icon" />
+           
+//                 </a>
+//                 </a>  
+//                         <div className="nav__search-icon" >  
+//                            <a href="#" className="nav__search"> Search Products & brands 
+//                            <a href="#" className="nav__search-icon2">
+//                             <FaSistrix className="nav__search-icon2" />
+                          
+//                            </a>
+//                            </a>            
+//                         </div>   
+//             <u1 className="nav__menu">
+//                 <li className="nav__item"><a href className="nav__link">Download App</a></li>
+//                 <li className="nav__item"><a href className="nav__link">Signup/login</a></li>
+//             </u1>
 
            
-        </div>
+//         </div>
         
             
-    );
-}
+//     );
+// }
 
+
+// export default Body;
+
+import React from "react";
+import "./body.css";
+import { FaBars, FaSistrix } from "react-icons/fa";
+
+function Body() {
+  return (
+    <div className="nav">
+      <a href="#" className="nav__brand">
+        Shopio
+        <a href="shopio" className="nav__brand-icon">
+          <FaBars className="nav__brand-icon" />
+        </a>
+      </a>
+      <div className="nav__search-icon">
+        <a href="#" className="nav__search">
+          Search Products & brands
+          <a href="#" className="nav__search-icon2">
+            <FaSistrix className="nav__search-icon2" />
+          </a>
+        </a>
+      </div>
+      <ul className="nav__menu">
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Download App
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Signup/login
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 export default Body;
